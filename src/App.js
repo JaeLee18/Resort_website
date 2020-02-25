d1001805d326d968c48f66b0fefdcda99364f55c
JaeLee18 <lee2161@purdue.edu>
2020-02-25 22:27:58 +0900

    Implmented Navbar and its elements

--- src/App.js
+++ src/App.js
@@ -8,12 +8,19 @@ import Error from './pages/Error';
 
 import {Route, Switch} from 'react-router-dom';
 
+import Navbar from './components/Navbar'
+
+
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/rooms/" component={Rooms}/>
-      <Route exact path="/single-room" component={SingleRoom}/>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms/" component={Rooms}/>
+        <Route exact path="/rooms/:slug" component={SingleRoom}/>
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
