b2369162d683e36d0e536969ead2364b22a92703
JaeLee18 <lee2161@purdue.edu>
2020-02-25 23:08:04 +0900

    Set up all banners

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Hero from '../components/Heros'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 
 export default function Home() {
     return (
         <Hero >
-            
+            <Banner title="luxurious rooms" subtitle="deluxe rooms starting $299">
+            <Link to='/rooms' className="btn-primary">
+                our rooms
+            </Link>
+            </Banner>
         </Hero>
     )
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Hero from '../components/Heros'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 
 export const Rooms = () => {
     return (
-        <Hero hero="roomsHero" />
-
-        
+        <Hero hero="roomsHero">
+            <Banner title="out rooms">
+                <Link to='/' className='btn-primary'>
+                    return home
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
 
