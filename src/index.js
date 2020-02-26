3b8830bd39148d9276047069ab4193cbb6151845
JaeLee18 <lee2161@purdue.edu>
2020-02-26 19:09:38 +0900

    Added a context

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDom from 'react-dom'
 // import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App'
+import {RoomProvider} from './context'
 
 ReactDom.render(
+  <RoomProvider>
     <Router>
         <App />
     </Router>
+  </RoomProvider>
   ,
   document.querySelector('#root')
 )
